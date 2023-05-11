e5cee2281fe49cd53e63cf8a230aa508ef0a5912
Luke Rogerson <luke@orbs.com>
2023-05-11 14:14:31 +0100

    Complete first draft ERC20 token

--- contracts/Shaluke.sol
+++ contracts/Shaluke.sol
@@ -3,54 +3,73 @@ pragma solidity ^0.8.9;
 import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import {IERC20Metadata} from '@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol';
 
+import 'hardhat/console.sol';
+
 contract ShalukeToken is IERC20, IERC20Metadata {
     // TODO uint256?
     mapping(address => uint) public balances;
-    string tokenName = 'Shaluke';
-    string tokenSymbol = 'SHALUKE';
+    mapping(address => mapping(address => uint)) public allowances;
+
+    string constant TOKEN_NAME = 'Shaluke';
+    string constant TOKEN_SYMBOL = 'SHALUKE';
+    uint256 immutable TOTAL_SUPPLY;
 
     constructor(uint256 initialSupply) {
         balances[msg.sender] = initialSupply;
+        TOTAL_SUPPLY = initialSupply;
     }
 
     function transfer(address to, uint256 amount) external returns (bool) {
-        if (balances[msg.sender] < amount) {
+        _transfer(msg.sender, to, amount);
+    }
+
+    function _transfer(address from, address to, uint256 amount) private returns (bool) {
+        if (balances[from] < amount) {
             return false;
         }
 
-        balances[msg.sender] -= amount;
+        balances[from] -= amount;
         balances[to] += amount;
 
-        emit Transfer(msg.sender, to, amount);
+        emit Transfer(from, to, amount);
         return true;
     }
 
     function totalSupply() external view returns (uint256) {
-        return 1;
+        return TOTAL_SUPPLY;
     }
 
     function balanceOf(address account) external view returns (uint256) {
-        return 1;
+        return balances[account];
     }
 
     function allowance(address owner, address spender) external view returns (uint256) {
-        return 1;
+        return allowances[owner][spender];
     }
 
+    // Should this ever fail?
     function approve(address spender, uint256 amount) external returns (bool) {
+        // No need to check balance of sender here?
+        allowances[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
         return true;
     }
 
     function transferFrom(address from, address to, uint256 amount) external returns (bool) {
-        return true;
+        if (allowances[from][msg.sender] < amount) {
+            return false;
+        }
+
+        allowances[from][msg.sender] -= amount;
+        _transfer(from, to, amount);
     }
 
     function name() external view returns (string memory) {
-        return tokenName;
+        return TOKEN_NAME;
     }
 
     function symbol() external view returns (string memory) {
-        return tokenSymbol;
+        return TOKEN_SYMBOL;
     }
 
     function decimals() external view returns (uint8) {
--- test/Shaluke.ts
+++ test/Shaluke.ts
@@ -4,13 +4,13 @@ import { expect } from 'chai';
 
 function deployShalukeFixture(initialBalance: number) {
     const inner = async () => {
-        const [owner, otherAccount] = await ethers.getSigners();
+        const [owner, otherAccount, thirdAccount] = await ethers.getSigners();
         const ShalukeToken = await ethers.getContractFactory('ShalukeToken');
 
         // TODO add name for the contract, other init params?
         const shalukeToken = await ShalukeToken.deploy(ethers.utils.parseUnits(String(initialBalance), 'ether'));
 
-        return { shalukeToken, owner, otherAccount };
+        return { shalukeToken, owner, otherAccount, thirdAccount };
     };
 
     return inner;
@@ -38,5 +38,109 @@ describe('transfer', () => {
 
         const res = await shalukeToken.transfer(otherAccount.address, amount);
         await expect(res).to.emit(shalukeToken, 'Transfer').withArgs(owner.address, otherAccount.address, amount);
+    });
+});
+
+describe('balanceOf', () => {
+    it('should return the initial balance for deployer', async () => {
+        const { shalukeToken, owner } = await loadFixture(deployShalukeFixture(500));
+        expect(await shalukeToken.balanceOf(owner.address)).to.equal(ethers.utils.parseUnits('500', 'ether'));
+    });
+    it('should return the initial balance for non-deployer', async () => {
+        const { shalukeToken, otherAccount } = await loadFixture(deployShalukeFixture(500));
+        expect(await shalukeToken.balanceOf(otherAccount.address)).to.equal(0);
+    });
+    // This is combining unit and integaration tests functionality. Is this typical in Solidity?
+    it('should reflect balance after transfer', async () => {
+        const { shalukeToken, otherAccount, owner } = await loadFixture(deployShalukeFixture(500));
+        await shalukeToken.transfer(otherAccount.address, ethers.utils.parseUnits('100', 'ether'));
+        expect(await shalukeToken.balanceOf(otherAccount.address)).to.equal(ethers.utils.parseUnits('100', 'ether'));
+        expect(await shalukeToken.balanceOf(owner.address)).to.equal(ethers.utils.parseUnits('400', 'ether'));
+    });
+});
+
+describe('totalSupply', () => {
+    it('should return the initial balance', async () => {
+        const { shalukeToken } = await loadFixture(deployShalukeFixture(1000));
+
+        const totalSupply = await shalukeToken.totalSupply();
+        expect(totalSupply).to.equal(ethers.utils.parseUnits('1000', 'ether'));
+    });
+});
+
+describe('approve', () => {
+    it('should allow a user to approve another to spend on their behalf', async () => {
+        const { shalukeToken, owner, otherAccount } = await loadFixture(deployShalukeFixture(1000));
+
+        const res = shalukeToken.approve(otherAccount.address, ethers.utils.parseUnits('100', 'ether'));
+        await expect(res)
+            .to.emit(shalukeToken, 'Approval')
+            .withArgs(owner.address, otherAccount.address, ethers.utils.parseUnits('100', 'ether'));
+    });
+});
+
+// no allowance
+// not enough allowance
+// with enough allowance
+describe('transferFrom', () => {
+    it('should not allow a transfer when user has not setup an allowance for that user', async () => {
+        const { shalukeToken, owner, otherAccount, thirdAccount } = await loadFixture(deployShalukeFixture(1000));
+
+        const res = await shalukeToken
+            .connect(otherAccount)
+            .transferFrom(owner.address, thirdAccount.address, ethers.utils.parseUnits('100', 'ether'));
+
+        await expect(res).to.not.emit(shalukeToken, 'Transfer');
+    });
+
+    it('should not allow a transfer when user does not have enough allowance', async () => {
+        const { shalukeToken, owner, otherAccount, thirdAccount } = await loadFixture(deployShalukeFixture(1000));
+
+        await shalukeToken.approve(otherAccount.address, ethers.utils.parseUnits('50', 'ether'));
+
+        const res = await shalukeToken
+            .connect(otherAccount)
+            .transferFrom(owner.address, thirdAccount.address, ethers.utils.parseUnits('100', 'ether'));
+
+        await expect(res).to.not.emit(shalukeToken, 'Transfer');
+    });
+
+    it('should allow a transfer when user has sufficient allowance', async () => {
+        const { shalukeToken, owner, otherAccount, thirdAccount } = await loadFixture(deployShalukeFixture(1000));
+        await shalukeToken.approve(otherAccount.address, ethers.utils.parseUnits('100', 'ether'));
+
+        const res = await shalukeToken
+            .connect(otherAccount)
+            .transferFrom(owner.address, thirdAccount.address, ethers.utils.parseUnits('50', 'ether'));
+
+        await expect(res)
+            .to.emit(shalukeToken, 'Transfer')
+            .withArgs(owner.address, thirdAccount.address, ethers.utils.parseUnits('50', 'ether'));
+    });
+
+    it('should reduce the allowance after a transfer', async () => {
+        const { shalukeToken, owner, otherAccount, thirdAccount } = await loadFixture(deployShalukeFixture(1000));
+
+        await shalukeToken.approve(otherAccount.address, ethers.utils.parseUnits('10', 'ether'));
+
+        expect(
+            await shalukeToken
+                .connect(otherAccount)
+                .transferFrom(owner.address, thirdAccount.address, ethers.utils.parseUnits('2', 'ether')),
+        )
+            .to.emit(shalukeToken, 'Transfer')
+            .withArgs(owner.address, thirdAccount.address, ethers.utils.parseUnits('2', 'ether'));
+
+        expect(
+            await shalukeToken
+                .connect(otherAccount)
+                .transferFrom(owner.address, thirdAccount.address, ethers.utils.parseUnits('8', 'ether')),
+        )
+            .to.emit(shalukeToken, 'Transfer')
+            .withArgs(owner.address, thirdAccount.address, ethers.utils.parseUnits('8', 'ether'));
+
+        expect(
+            await shalukeToken.connect(otherAccount).transferFrom(owner.address, thirdAccount.address, 1),
+        ).to.not.emit(shalukeToken, 'Transfer');
     });
 });
